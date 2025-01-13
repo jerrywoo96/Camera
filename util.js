69e06ee30e9ff27dccd0ff972e18bd282f6f6f26
Jerry Woo CW <jerrywoo96@users.noreply.github.com>
2025-01-14 00:50:25 +0800

    update

---
+++ util.js
@@ -0,0 +1,30 @@
+function csvToJson(csv) {
+  const header = [];
+  const data = csv_parse_sync.parse(csv, {
+    columns: function (columns) {
+      header.push(...columns);
+      return columns;
+    },
+    skip_empty_lines: true
+  });
+  return {
+    header,
+    data
+  };
+}
+
+function getCSV(url) {
+  return new Promise(function (res) {
+    $.get(url, function (data, status) {
+      res(csvToJson(data));
+    });
+  });
+}
+
+function listToTable(id, list, headers) {
+  const table = document.getElementById(id);
+  const thead = `<thead>${headers.map(header => `<th>${header}</th>`).join('')}</thead>`;
+  const tbody = `<tbody>${list.map(row => `<tr>${headers.map(header => `<td>${row[header]}</td>`).join('')}</tr>`).join('')}</tbody>`;
+  table.innerHTML = thead + tbody;
+  Sortable.initTable(table)
+}
